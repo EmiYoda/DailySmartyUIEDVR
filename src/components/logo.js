310ee21164fb1814ecc67e5c5b8d3d306747d379
codigo sesion <emilianodelvalle@icloud.com>
2020-06-25 15:47:46 -0500

    setup redux form in the search bar

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 
-
-export default class Logo extends Component {
-  render() {
-    return (
-      <div className="logo-main">
-        <img alt="daily smarty ui logo big" src="/assets/ds_circle_logo.png"/>
-      </div>
-    );
-  }
+class Logo extends Component {
+    render() {
+        return (
+            <div className="logo-main">
+                <img alt="daily smarty ui image logo big" src="/assets/ds_circle_logo.png"/>
+            </div>
+        )
+    }
 }
+
+export default Logo;
--- src/components/recentPosts.js
+++ src/components/recentPosts.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
 
-
-export default class RecentPosts extends Component {
-  render() {
-    return (
-        <div className="recent-posts">
-            <div className="recent-posts-wrapper">
-                <div className="recent-posts-heading">
-                    <ul className="recent-posts-posts">
+class SearchBar extends Component {
+    render() {
+        return (
+            <div className="recent-posts">
+                <div className="recent-posts__wrapper">
+                    <div className="recent-posts__heading">Recent Posts</div>
+                    <ul className="recent-posts__posts">
                         <li>recent post 0</li>
                         <li>recent post 1</li>
                         <li>recent post 2</li>
                     </ul>
                 </div>
+
             </div>
-        </div>
-    );
-  }
+        )
+    }
 }
+
+export default SearchBar;
